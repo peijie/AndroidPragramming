2a22a924b784287188faa8389448c46ce3de22de
peijie <peijie_wang@hotmail.com>
2015-01-25 11:31:11 +0800

    Implemented display text activity. user can send and display text

--- app/src/androidTest/java/com/mountainwind/myfirstapp/ApplicationTest.java
+++ app/src/androidTest/java/com/mountainwind/myfirstapp/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.mountainwind.helloworld;
+package com.mountainwind.myfirstapp;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/mountainwind/myfirstapp/MainActivity.java
+++ app/src/main/java/com/mountainwind/myfirstapp/MainActivity.java
@@ -1,12 +1,16 @@
-package com.mountainwind.helloworld;
+package com.mountainwind.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "com.mountainwind.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,15 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+
+        EditText editText = (EditText)findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +49,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
